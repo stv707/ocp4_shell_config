9412dcf92c93f4a42cf16fdd1237668fb9f2af5d
stv707 <stv707@gmail.com>
2021-07-04 09:08:11 +0800

    v2

---
+++ .oc-prompt.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+__oc_ps1()
+{
+    oc status &> /dev/null
+    if [ $? -eq 0 ]
+    then
+    # Get current context
+    CONTEXT=$(cat ~/.kube/config 2>/dev/null| grep -o '^current-context: [^/]*' | cut -d' ' -f2)
+
+    if [ -n "$CONTEXT" ]; then
+        echo "(ocp:${CONTEXT})"
+    fi
+    else
+            echo "(ocp:NOTLOGGEDIN)"
+    fi
+}
+
+
+__oc_user()
+{
+        oc status &> /dev/null
+        if [ $? -eq 0 ]
+        then
+
+        GG=$(oc whoami)
+        if [ -n "$GG" ]
+        then
+                echo "(ocu:${GG})"
+        fi
+
+        else
+        echo "(ocu:NOTLOGGEDIN)"
+        fi
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# ocp4_shell_config
-Making your life easier with setting shell prompt to show OCP user and project/ns 
+# ocp4 shell config
+***
+> Making your life easier with setting shell prompt to show OCP user and project/ns 
+
+* Steps:
+
+1. cd to your home dir 
+```
+cd ~
+``` 
+
+2. git clone this repo
+```
+git clone https://github.com/stv707/ocp4_shell_config.git 
+```
+
+3. run the shell script to set .bashrc and copy .oc_prompt.sh  
+```
+bash ocp4_shell_config/runme.sh 
+```
+
+4. Log out and Login 
+```
+exit
+```
+
